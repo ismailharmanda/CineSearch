bf1fec098edcd89605011bf13885bf6fd6e17395
İsmail Harmanda <iharmanda@hotmail.com>
2024-10-08 11:43:29 +0300

    chore: add useFavorites and useFavoriteMutation to exported hooks

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,3 +2,5 @@ export {useNetworkConnectivity} from './useNetworkConnectivity';
 export {usePopularMovies} from './usePopularMovies';
 export {useSearchMovies} from './useSearchMovies';
 export {useMovieDetails} from './useMovieDetails';
+export {useFavorites} from './useFavorites';
+export {useFavoriteMutation} from './useFavoriteMutation';
